5dc04f0a8b78484951d1887ec94522e1384e5c68
yogeee12 <yogeshlohar129@gmail.com>
2026-09-25 17:20:02 +0530

    convert str data into JSON formate and rename file name

--- backend/ai_gen_result.py
+++ backend/ai_gen_result.py
@@ -3,24 +3,28 @@ import os
 from dotenv import load_dotenv
 from google import genai
 from data import company, candidate, responsibility
+import json
 
 load_dotenv()
-
-client = genai.Client(
-    api_key=os.getenv("GEMINI_API_KEY")
-)
-
-response = client.models.generate_content(
-    model="gemini-3.5-flash",
-    contents=f"""
-    You are an experienced HR and technical interviewer for NovaTech Labs.
-
-    COMPANY INFORMATION : {company}
-
-    CANDIDATE INFORMATION : {candidate}
-    
-    {responsibility}
-    """
+def ai_result():
+    client = genai.Client(
+        api_key=os.getenv("GEMINI_API_KEY")
     )
 
-print(response.text)
+    response = client.models.generate_content(
+        model="gemini-3.5-flash",
+        contents=f"""
+        You are an experienced HR and technical interviewer for NovaTech Labs.
+
+        COMPANY INFORMATION : {company}
+
+        CANDIDATE INFORMATION : {candidate}
+        
+        {responsibility}
+        """
+        )
+
+    str_data = response.text
+    data = json.loads(str_data)
+
+    return data
